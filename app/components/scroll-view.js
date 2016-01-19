1fdd6a3a7cdb514a76c13190cfe716b7654eab19
Ali Zahid <ali.zahid@live.com>
2016-01-19 13:04:08 +0500

    Scroll view component added

---
+++ app/components/scroll-view.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'section',
+
+	didInsertElement: function () {
+		var start = this.start,
+			end = this.end;
+
+		var timer;
+
+		this.$().on('scroll', function () {
+			if (!timer) {
+				if (typeof start === 'function') {
+					start();
+				}
+			}
+
+			if (timer) {
+				Ember.run.cancel(timer);
+			}
+
+			timer = Ember.run.later(function () {
+				if (typeof end === 'function') {
+					end();
+				}
+
+				timer = null;
+			}, 500);
+		});
+	}
+});
