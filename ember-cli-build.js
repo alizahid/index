66fc5b426d9919f053f38948804efcf3bdcf5b2c
Ali Zahid <ali.zahid@live.com>
2016-01-19 07:24:06 +0500

    Configuration and packages added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
 	var app = new EmberApp(defaults, {
-
+		storeConfigInMeta: false,
+		fingerprint: {
+			enabled: false
+		},
+		autoprefixer: {
+			browsers: ['last 2 version']
+		}
 	});
+
+	app.import('vendor/scripts/shortid.min.js');
 
 	return app.toTree();
 };
