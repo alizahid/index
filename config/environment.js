64d3aa49fd4772530cb94fd02d4362fca3f19c54
Ali Zahid <ali.zahid@live.com>
2016-01-26 02:52:51 +0500

    Refactoring

--- config/environment.js
+++ config/environment.js
@@ -14,13 +14,7 @@ module.exports = function (environment) {
 		contentSecurityPolicyMeta: true
 	};
 
-	if (environment === 'development') {
-		//ENV.APP.LOG_RESOLVER = true;
-		//ENV.APP.LOG_ACTIVE_GENERATION = true;
-		//ENV.APP.LOG_TRANSITIONS = true;
-		//ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-		//ENV.APP.LOG_VIEW_LOOKUPS = true;
-	}
+	if (environment === 'development') {}
 
 	if (environment === 'test') {
 		ENV.baseURL = '/';
