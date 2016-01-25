5ade893516b9df51decb378b9c3b9fa72c92ca11
Ali Zahid <ali.zahid@live.com>
2016-01-26 01:27:59 +0500

    Date time textfield implemented

---
+++ app/initializers/reopen-textfield.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+export function initialize() {
+	Ember.TextField.reopen({
+		valueChanged: function () {
+			if (this.$()[0].type === 'datetime-local') {
+				Ember.run.next(this, function () {
+					var date = moment(this.value).toDate(),
+						timezoneOffset = date.getTimezoneOffset() * 60 * 1000;
+
+					date = new Date(date.getTime() - timezoneOffset).toISOString();
+					date = date.substr(0, date.lastIndexOf(':'));
+
+					this.$().val(date);
+				});
+			}
+		}.on('didInsertElement').observes('value')
+	})
+}
+
+export default {
+	name: 'reopen-textfield',
+	initialize
+};
