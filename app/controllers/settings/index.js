09b6d1950580262ef3c2c721d113b2c5e9306482
Ali Zahid <ali.zahid@live.com>
2016-01-27 06:02:29 +0500

    Export data feature implemented

--- app/controllers/settings/index.js
+++ app/controllers/settings/index.js
@@ -3,7 +3,37 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	actions: {
 		exportData: function () {
+			var data = this.store.adapterFor('application').data;
 
+			if (data.item && data.item.length > 0) {
+				try {
+					navigator.notification.prompt('Please enter your email. We will send the link here.', function (response) {
+						var button = response.buttonIndex,
+							email = response.input1;
+
+						if (button === 1) {
+							$.ajax('https://designplox.com/index/data/?export', {
+								dataType: 'json',
+								method: 'POST',
+								data: JSON.stringify({
+									email: email,
+									data: data.item
+								})
+							}).then(function (data) {
+								try {
+									navigator.notification.alert(data.message, null, 'Success');
+								} catch (ex) {}
+							}, function (xhr) {
+								try {
+									navigator.notification.alert(xhr.responseJSON ? xhr.responseJSON.message : 'A server error occurred. Please try again later.', null, 'Error');
+								} catch (ex) {}
+							});
+						}
+					}, 'Export data', ['Send', 'Cancel']);
+				} catch (ex) {}
+			} else {
+				navigator.notification.alert('You have no data to export right now.', null, 'Export');
+			}
 		},
 		clearData: function () {
 			try {
