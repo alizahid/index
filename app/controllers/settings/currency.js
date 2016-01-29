65915520c97b5aa07b3394d0c63be26239e26d26
Ali Zahid <ali.zahid@live.com>
2016-01-29 14:49:35 +0500

    Currency search added

--- app/controllers/settings/currency.js
+++ app/controllers/settings/currency.js
@@ -1,8 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	currencies: function () {
+		var query = this.query || '',
+			data = this.model;
+
+		return Ember.ArrayProxy.extend({
+			arrangedContent: Ember.computed.sort('content', 'props'),
+			props: ['selected:desc', 'name:asc']
+		}).create({
+			content: data.filter(function (currency) {
+				if (currency.name.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+					return true;
+				}
+			})
+		});
+	}.property('query'),
 	actions: {
 		selectCurrency: function (currency) {
+			this.set('query', null);
+
+			Scroll.top();
+
 			this.model.setEach('selected', false);
 
 			Ember.set(currency, 'selected', true);
