3acbda80370ef2c41aefe793fe0f741e3012966f
Ali Zahid <ali.zahid@live.com>
2016-03-04 00:03:16 +0500

    Ember CLI updated

--- app/initializers/cordova.js
+++ app/initializers/cordova.js
@@ -1,23 +1,25 @@
+export function initialize(app) {
+	app.deferReadiness();
+
+	document.addEventListener('deviceready', function() {
+		try {
+			AppRate.preferences.storeAppURL.ios = '1078789240';
+			AppRate.preferences.storeAppURL.android = 'market://details?id=io.index';
+
+			AppRate.preferences.customLocale = {
+				title: 'Rate Index',
+				message: 'If you found Index useful, please take a moment to rate the app. Thanks!',
+				cancelButtonLabel: 'No, thanks',
+				laterButtonLabel: 'Remind me later',
+				rateButtonLabel: 'Rate it now'
+			};
+		} catch (ex) {}
+
+		app.advanceReadiness();
+	}, false);
+}
+
 export default {
 	name: 'cordova',
-	initialize: function (app) {
-		app.deferReadiness();
-
-		document.addEventListener('deviceready', function () {
-			try {
-				AppRate.preferences.storeAppURL.ios = '1078789240';
-				AppRate.preferences.storeAppURL.android = 'market://details?id=io.index';
-
-				AppRate.preferences.customLocale = {
-					title: 'Rate Index',
-					message: 'If you found Index useful, please take a moment to rate the app. Thanks!',
-					cancelButtonLabel: 'No, thanks',
-					laterButtonLabel: 'Remind me later',
-					rateButtonLabel: 'Rate it now'
-				};
-			} catch (ex) {}
-
-			app.advanceReadiness();
-		}, false);
-	}
+	initialize
 };
