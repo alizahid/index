9d773cca2a5903bf3519137f78b242c0658a53c9
Ali Zahid <ali.zahid@live.com>
2016-03-04 13:29:40 +0500

    Model updated

--- app/models/account.js
+++ app/models/account.js
@@ -1,6 +1,11 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
 	name: DS.attr('string'),
-	items: DS.hasMany('item')
+	items: DS.hasMany('item'),
+
+	isDefault: Ember.computed(function() {
+		return this.id === 'default';
+	})
 });
