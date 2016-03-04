0e5c5fca0b59916397cfeb45c62a13adaca1e01b
Ali Zahid <ali.zahid@live.com>
2016-03-04 12:48:32 +0500

    Models updated

--- app/models/item.js
+++ app/models/item.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	account: DS.belongsTo('account'),
 	type: DS.attr('string'),
 	category: DS.attr('string'),
 	description: DS.attr('string'),
