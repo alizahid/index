3322922818c3de34e49a00083c91a1fac130eb73
Ali Zahid <ali.zahid@live.com>
2016-03-04 12:58:26 +0500

    Migration helper added

--- vendor/scripts/spinner.js
+++ vendor/scripts/spinner.js
@@ -15,3 +15,21 @@ var Spinner = new function() {
 		spinner.stop(true, true).fadeOut('fast');
 	};
 };
+
+var Migration = new function() {
+	var spinner = $('<div class="migration"><div class="overlay"><span></span><div class="spinner"><div class="mask">').appendTo('body');
+
+	this.show = function(message) {
+		if (message) {
+			$('span', spinner).text(message);
+		}
+
+		spinner.stop(true, true).fadeIn('fast');
+	};
+
+	this.hide = function() {
+		$('span', spinner).text('');
+
+		spinner.stop(true, true).fadeOut('fast');
+	};
+};
