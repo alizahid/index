bc122b2d829b726893027ac6e347b236973bec37
Ali Zahid <ali.zahid@live.com>
2016-03-05 23:12:21 +0500

    Refactoring

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,30 +1,32 @@
 import Ember from 'ember';
 
 export function formatCurrency([currency, amount, roundDecimalIfZero]) {
-	let whole = Math.abs(parseInt(amount)).toString(),
-		decimal = parseFloat(amount).toFixed(2),
-		negative = amount < 0,
-		length = whole.length;
+	if (currency) {
+		let whole = Math.abs(parseInt(amount)).toString(),
+			decimal = parseFloat(amount).toFixed(2),
+			negative = amount < 0,
+			length = whole.length;
 
-	if (length > 3) {
-		let prefix = whole.substr(0, length % 3);
+		if (length > 3) {
+			let prefix = whole.substr(0, length % 3);
 
-		whole = prefix + (prefix.length > 0 ? ',' : '') + whole.substr(length % 3).replace(/(\d{3})(?=\d)/g, '$1,');
-	}
-
-	let data = '<span class="symbol">' + currency.get('print') + '</span>' + (negative ? ' -' : '') + whole;
-
-	if (roundDecimalIfZero) {
-		decimal = decimal.slice(decimal.indexOf('.'));
-
-		if (parseFloat(decimal) > 0) {
-			data += decimal;
+			whole = prefix + (prefix.length > 0 ? ',' : '') + whole.substr(length % 3).replace(/(\d{3})(?=\d)/g, '$1,');
 		}
-	} else {
-		data += decimal.slice(decimal.indexOf('.'));
-	}
 
-	return Ember.String.htmlSafe(data);
+		let data = '<span class="symbol">' + currency.get('print') + '</span>' + (negative ? ' -' : '') + whole;
+
+		if (roundDecimalIfZero) {
+			decimal = decimal.slice(decimal.indexOf('.'));
+
+			if (parseFloat(decimal) > 0) {
+				data += decimal;
+			}
+		} else {
+			data += decimal.slice(decimal.indexOf('.'));
+		}
+
+		return Ember.String.htmlSafe(data);
+	}
 }
 
 export default Ember.Helper.helper(formatCurrency);
