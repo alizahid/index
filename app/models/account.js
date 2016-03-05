d4a8f08bd8930bbedf5a140e623588214e49b091
Ali Zahid <ali.zahid@live.com>
2016-03-05 21:42:22 +0500

    Models updated

--- app/models/account.js
+++ app/models/account.js
@@ -4,6 +4,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	items: DS.hasMany('item'),
+	currency: DS.belongsTo('currency'),
 
 	isDefault: Ember.computed(function() {
 		return this.id === 'default';
