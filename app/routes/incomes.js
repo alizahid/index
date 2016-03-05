08cf640ce9b2cd1538ca637f4c20155c1ee09974
Ali Zahid <ali.zahid@live.com>
2016-03-06 04:27:02 +0500

    Skip account selection if only one account

--- app/routes/incomes.js
+++ app/routes/incomes.js
@@ -4,11 +4,13 @@ export default Ember.Route.extend({
 	model() {
 		return this.store.findAll('account');
 	},
-	setupController(controller) {
+	setupController(controller, model) {
 		this._super(...arguments);
 
+		var onlyOne = model.get('length') === 1;
+
 		controller.setProperties({
-			account: null,
+			account: onlyOne ? model.get('firstObject') : null,
 			category: null,
 			item: {}
 		});
