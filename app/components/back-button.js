93d177bd4bcb45724f06da4048949a0b2f2f6b0b
Ali Zahid <ali.zahid@live.com>
2016-03-08 09:06:39 +0500

    Refactoring

--- app/components/back-button.js
+++ app/components/back-button.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	routing: Ember.inject.service('-routing'),
+
 	tagName: 'a',
 	classNames: ['back'],
 	click() {
 		if (window.history.length > 1) {
 			window.history.back();
 		} else {
-			this.get('controller').transitionToRoute('index');
+			this.get('routing').transitionTo('index');
 		}
 	}
 });
